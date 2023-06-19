71e1d83d47292a835fc2b7538e469df4670314a0
sachinnsendhav <sachin.sendhav@codesquaretech.com>
2023-06-19 15:52:51 +0530

    manage token on login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,9 +1,8 @@
 import Layout from '../layouts/Main';
 import Link from 'next/link';
 import { useForm } from "react-hook-form";
-import { server } from '../utils/server';
-import { postData } from '../utils/services';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react'
 type LoginMail = {
   email: string;
   password: string;
@@ -12,6 +11,15 @@ type LoginMail = {
 const LoginPage = () => {
   const { register, handleSubmit, errors } = useForm();
   const router = useRouter();
+  const [authStatus, setAuthStatus] = useState("false")
+  const [authToken, setAuthToken] = useState("");
+  const [userDetails, setUserDetails] = useState()
+  console.log("authStatus", authStatus, "token->", authToken)
+  useEffect(() => {
+    setAuthStatus(localStorage.getItem("status"));
+    setAuthToken(localStorage.getItem("token"))
+  }, [])
+
   const onSubmit = async (data: LoginMail) => {
     var formdata = new FormData();
     formdata.append('grant_type', "password");
@@ -27,86 +35,159 @@ const LoginPage = () => {
     );
     const result = await resp.json();
     localStorage.setItem("token", result?.access_token)
-    if(result){
-      router.push('/');
-    }
+    localStorage.setItem("status", "true")
+    // if (result) {
+    //   router.push('/');
+    // }
   };
+  const checkTokenExpiry = async () => {
+    if (authToken) {
+      const data =
+      {
+        "data": {
+          "type": "access-tokens",
+          "attributes": {
+            "username": "sonia@spryker.com",
+            "password": "change123"
+          }
+        }
+      }
+      try {
+        const resp = await fetch(
+          `https://glue.de.faas-suite-prod.cloud.spryker.toys/access-tokens`,
+          {
+            method: 'POST',
+            headers: {
+              "Authorization": `Bearer ${authToken}`
+            },
+            body: JSON.stringify(data),
+          },
+        );
+        const result = await resp.json();
+        console.log("result-->>", result)
+
+        localStorage.setItem("status", "true")
+        localStorage.setItem("token", result?.data?.attributes?.accessToken)
+
+      } catch (err) {
+        console.log("errrrrrrrrrrrrrr", err)
+        localStorage.setItem("status", "false")
+      }
+
+    } else {
+      localStorage.setItem("status", "false")
+    }
+
+  }
+
+  const getUserDetails = async () => {
+    if (authToken) {
+      const resp = await fetch(`https://glue.de.faas-suite-prod.cloud.spryker.toys/customers`,
+        {
+          method: "GET",
+          headers: {
+            "Authorization": `Bearer ${authToken}`
+          }
+        }
+      );
+      const result = await resp.json();
+      setUserDetails(result.data[0].attributes)
+    }
+  }
+  console.log("user-details--", userDetails)
+  useEffect(() => {
+    checkTokenExpiry();
+    getUserDetails();
+  }, [authToken])
+
+
+
   return (
     <Layout>
       <section className="form-page">
         <div className="container">
           <div className="back-button-section">
-            <Link href="/products">
-              <a><i className="icon-left"></i> Back to store</a>
+            <Link href="/">
+              <a><i className="icon-left"></i> Back to Home</a>
             </Link>
           </div>
+          {authStatus === "true" ? <div>
 
-          <div className="form-block">
-            <h2 className="form-block__title">Log in</h2>
+            <h1 style={{fontSize:"18px", paddingBottom:"10px"}}>
+              User Details
+            </h1>
+            <p style={{paddingBottom:"5px"}}>Name : {userDetails?.firstName + " " + userDetails?.lastName}</p>
+            <p style={{paddingBottom:"5px"}}>Email : {userDetails?.email}</p>
+            <p style={{paddingBottom:"5px"}}>Gender : {userDetails?.gender}</p>
+          </div> :
 
-            <form className="form" onSubmit={handleSubmit(onSubmit)}>
-              <div className="form__input-row">
-                <input
-                  className="form__input"
-                  placeholder="sonia@spryker.com"
-                  type="text"
-                  name="email"
-                  value={"sonia@spryker.com"}//just for development testing please remove it after development
-                  ref={register({
-                    required: true,
-                    pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                  })}
-                />
+            <div className="form-block">
+              <h2 className="form-block__title">Log in</h2>
 
-                {errors.email && errors.email.type === 'required' &&
-                  <p className="message message--error">This field is required</p>
-                }
+              <form className="form" onSubmit={handleSubmit(onSubmit)}>
+                <div className="form__input-row">
+                  <input
+                    className="form__input"
+                    placeholder="sonia@spryker.com"
+                    type="text"
+                    name="email"
+                    value={"sonia@spryker.com"}//just for development testing please remove it after development
+                    ref={register({
+                      required: true,
+                      pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                    })}
+                  />
 
-                {errors.email && errors.email.type === 'pattern' &&
-                  <p className="message message--error">Please write a valid email</p>
-                }
-              </div>
+                  {errors.email && errors.email.type === 'required' &&
+                    <p className="message message--error">This field is required</p>
+                  }
 
-              <div className="form__input-row">
-                <input
-                  className="form__input"
-                  type="password"
-                  placeholder="changer123"
-                  value={"change123"}  //just for development testing please remove it after development
-                  name="password"
-                  ref={register({ required: true })}
-                />
-                {errors.password && errors.password.type === 'required' &&
-                  <p className="message message--error">This field is required</p>
-                }
-              </div>
-
-              <div className="form__info">
-                <div className="checkbox-wrapper">
-                  <label htmlFor="check-signed-in" className={`checkbox checkbox--sm`}>
-                    <input
-                      type="checkbox"
-                      name="keepSigned"
-                      id="check-signed-in"
-                      ref={register({ required: false })}
-                    />
-                    <span className="checkbox__check"></span>
-                    <p>Keep me signed in</p>
-                  </label>
+                  {errors.email && errors.email.type === 'pattern' &&
+                    <p className="message message--error">Please write a valid email</p>
+                  }
                 </div>
-                <a href="/forgot-password" className="form__info__forgot-password">Forgot password?</a>
-              </div>
 
-              {/* <div className="form__btns">
-                <button type="button" className="btn-social fb-btn"><i className="icon-facebook"></i>Facebook</button>
-                <button type="button" className="btn-social google-btn"><img src="/images/icons/gmail.svg" alt="gmail" /> Gmail</button>
-              </div> */}
+                <div className="form__input-row">
+                  <input
+                    className="form__input"
+                    type="password"
+                    placeholder="changer123"
+                    value={"change123"}  //just for development testing please remove it after development
+                    name="password"
+                    ref={register({ required: true })}
+                  />
+                  {errors.password && errors.password.type === 'required' &&
+                    <p className="message message--error">This field is required</p>
+                  }
+                </div>
 
-              <button type="submit" className="btn btn--rounded btn--yellow btn-submit">Sign in</button>
+                <div className="form__info">
+                  <div className="checkbox-wrapper">
+                    <label htmlFor="check-signed-in" className={`checkbox checkbox--sm`}>
+                      <input
+                        type="checkbox"
+                        name="keepSigned"
+                        id="check-signed-in"
+                        ref={register({ required: false })}
+                      />
+                      <span className="checkbox__check"></span>
+                      <p>Keep me signed in</p>
+                    </label>
+                  </div>
+                  <a href="/forgot-password" className="form__info__forgot-password">Forgot password?</a>
+                </div>
 
-              <p className="form__signup-link">Not a member yet? <a href="/register">Sign up</a></p>
-            </form>
-          </div>
+                {/* <div className="form__btns">
+    <button type="button" className="btn-social fb-btn"><i className="icon-facebook"></i>Facebook</button>
+    <button type="button" className="btn-social google-btn"><img src="/images/icons/gmail.svg" alt="gmail" /> Gmail</button>
+  </div> */}
+
+                <button type="submit" className="btn btn--rounded btn--yellow btn-submit">Sign in</button>
+
+                <p className="form__signup-link">Not a member yet? <a href="/register">Sign up</a></p>
+              </form>
+            </div>}
+
 
         </div>
       </section>

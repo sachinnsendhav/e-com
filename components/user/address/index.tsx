c4b2d41d65edec0deb91a80ebcb9f9b7bc6ab5fa
sachinnsendhav <sachin.sendhav@codesquaretech.com>
2023-07-04 11:02:44 +0530

    integrate api for delete addresses

--- components/user/address/index.tsx
+++ components/user/address/index.tsx
@@ -35,9 +35,75 @@ const Address = ({ show }: AddressType) => {
     useEffect(() => {
         getAddresses()
     }, [authToken])
+
+    const deleteAddress = async (id: any) => {
+        const resp = await fetch(`${API_URL}/customers/DE--21/addresses/${id}`, {
+            method: 'DELETE',
+            headers: {
+                authorization: `Bearer ${authToken}`
+            }
+        });
+        if (resp.status === 204) {
+            const updatedItems = address.filter((item: any) => item.id !== id);
+            setAddress(updatedItems || [])
+            alert("deleted sucessfully...!")
+        }
+    }
     return (
         <section style={style}>
-            {/* <div style={{
+            <div className="address-list" style={{ width: "90%", margin: "auto" }}>
+                <table>
+                    <tbody >
+                        <tr style={{ border: "1px solid black" }}>
+                            <th style={{ color: "black", paddingTop: "20px" }}>Name</th>
+                            <th style={{ color: "black", paddingTop: "20px" }}>Address</th>
+                            <th style={{ color: "black", paddingTop: "20px" }}>City</th>
+                            <th style={{ color: "black", paddingTop: "20px" }}>Country</th>
+                            <th style={{ color: "black", paddingTop: "20px" }}>Phone No.</th>
+                            <th style={{ color: "black", paddingTop: "20px" }}>Zipcode</th>
+                            <th style={{ color: "black", paddingTop: "20px" }}>Company</th>
+                            <th style={{ color: "black", paddingTop: "20px" }}>Action</th>
+
+                        </tr>
+                        {address.length > 0 ? address.map((item: any) => {
+                            return (
+                                <tr style={{ border: "1px solid black" }}>
+                                    <td>{`${item.attributes.salutation} ${item.attributes.firstName} ${item.attributes.lastName}`} </td>
+                                    <td>{`${item.attributes?.address1}, ${item.attributes?.address2}`}</td>
+                                    <td>{item.attributes.city}</td>
+                                    <td>{`${item.attributes.country}, (${item.attributes.iso2Code})`}</td>
+                                    <td>{item.attributes.phone}</td>
+                                    <td>{item.attributes.zipCode}</td>
+                                    <td>{item.attributes.company}</td>
+                                    <td><button style={{
+                                        backgroundColor: "black",
+                                        color: "white",
+                                        paddingInline: "10px",
+                                        padding: "5px",
+                                        borderRadius: "5px"
+                                    }} onClick={() => deleteAddress(item.id)}>Delete</button></td>
+
+                                </tr>)
+                        }) : null}
+                    </tbody>
+                </table>
+            </div>
+        </section>
+    )
+}
+
+export default Address
+
+
+
+
+
+
+
+
+
+
+{/* <div style={{
                 display: "flex",
                 flexWrap: "wrap",
             }}>
@@ -79,37 +145,3 @@ const Address = ({ show }: AddressType) => {
                     )
                 })}
             </div> */}
-            <div className="address-list" style={{ width: "90%", margin: "auto" }}>
-                <table>
-                    <tbody >
-                        <tr style={{ border: "1px solid black" }}>
-                            <th style={{ color: "black", paddingTop: "20px" }}>Name</th>
-                            <th style={{ color: "black", paddingTop: "20px" }}>Address</th>
-                            <th style={{ color: "black", paddingTop: "20px" }}>City</th>
-                            <th style={{ color: "black", paddingTop: "20px"}}>Country</th>
-                            <th style={{ color: "black", paddingTop: "20px" }}>Phone No.</th>
-                            <th style={{ color: "black", paddingTop: "20px" }}>Zipcode</th>
-                            <th style={{ color: "black", paddingTop: "20px" }}>Company</th>
-
-                        </tr>
-                        {address.length > 0 ? address.map((item: any) => {
-                            return (
-                                <tr style={{ border: "1px solid black" }}>
-                                    <td>{`${item.attributes.salutation} ${item.attributes.firstName} ${item.attributes.lastName}`} </td>
-                                    <td>{`${item.attributes?.address1}, ${item.attributes?.address2}`}</td>
-                                    <td>{item.attributes.city}</td>
-                                    <td>{`${item.attributes.country}, (${item.attributes.iso2Code})`}</td>
-                                    <td>{item.attributes.phone}</td>
-                                    <td>{item.attributes.zipCode}</td>
-                                    <td>{item.attributes.company}</td>
-
-                                </tr>)
-                        }) : null}
-                    </tbody>
-                </table>
-            </div>
-        </section>
-    )
-}
-
-export default Address

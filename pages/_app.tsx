4a756bd4615d837cf372a5dac4d1ac8b2179c99a
ashutoshnamdevcst <ashutosh.namdev@codesquaretech.com>
2023-07-05 17:08:57 +0530

    added wishlist on pdp page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,7 @@ import 'swiper/swiper.scss';
 import 'rc-slider/assets/index.css';
 import 'react-rater/lib/react-rater.css';
 import '../assets/css/styles.scss';
+import '../assets/css/content.css'
 
 import * as gtag from './../utils/gtag';
 

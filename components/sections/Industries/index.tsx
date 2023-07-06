8e357bb3b3617487c71edbc5d8b49b35143bf259
sachinnsendhav <sachin.sendhav@codesquaretech.com>
2023-07-06 16:54:16 +0530

    create blog section for home page

--- components/sections/Industries/index.tsx
+++ components/sections/Industries/index.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 
 function index(data:any) {
-  console.log("industries---data", data)
-
+  const newData = Array.from(Object.values(data));
+  const imageData: any = newData.pop();
+  const updatedArr: any[] = []
+  newData.forEach((element: any) => {
+    imageData.forEach((img: any) => {
+      if (element.image[0]?.sys?.id === img?.sys?.id) {
+        updatedArr.push({
+          image: `https:${img.fields.file.url}`,
+          description: element.description,
+          isLoggedIn: element.isLoggedIn,
+          title: element.title
+        })
+      }
+    });
+  });
+  console.log("industries-updatedArr", updatedArr)
   return (
-    <div>hi</div>
+    <div style={{backgroundColor:"#333", padding:"50px", paddingInline:"120px",color:"white"}}>
+      hello
+    </div>
   )
 }
 

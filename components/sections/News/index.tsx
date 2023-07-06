2356bff7244974fc7fcdca37b435407686f3165c
ashutoshnamdevcst <ashutosh.namdev@codesquaretech.com>
2023-07-06 18:51:37 +0530

    added news section

--- components/sections/News/index.tsx
+++ components/sections/News/index.tsx
@@ -1,10 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import moment from "moment";
 
-function index(data:any) {
+function index(data: any) {
+  const [finalData, setFinalData]= useState<any>();
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: <button className="slick-prev">Previous</button>,
+    nextArrow: <button className="slick-next">Next</button>,
+  };
+  console.log(data, "data");
+  const newData = Array.from(Object.values(data));
+  useEffect(() => {
+    if (newData) {
+      var temp = newData.slice(0,newData.length-2);
+      setFinalData(temp)
+    }
+  }, [newData]);
+
+  console.log(newData, "new data");
 
   return (
-    <div>hi</div>
-  )
+    <div className="containerParent">
+      <h1 className="headingTag">Ricoh News</h1>
+      <Slider {...settings}>
+        {finalData?.map((item: any, index: number) => (
+          <div className="newscard" key={index}>
+          <div >
+            <a href={item?.btnlink}>
+              <h3>{item?.description}</h3>
+              <h3 style={{paddingTop:'2rem'}}>{moment(item?.date).format("ddd, DD MMMM YYYY")}</h3>
+            </a>
+          </div>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
 }
 
-export default index
+export default index;

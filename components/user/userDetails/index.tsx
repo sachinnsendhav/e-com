4126a9ae21cc08e20ac7f4cb753b857e4b3a4e6b
sachinnsendhav <sachin.sendhav@codesquaretech.com>
2023-08-02 10:25:14 +0530

    update profile

--- components/user/userDetails/index.tsx
+++ components/user/userDetails/index.tsx
@@ -1,9 +1,23 @@
 import { API_URL } from "config";
 import React, { useState, useEffect } from "react"
 import Loader from '../../loader'
+import Modal from 'react-modal';
+
 type AddressType = {
   show: boolean;
 }
+const customStyles: any = {
+  content: {
+    top: '55%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    background: '#f0f0f0',
+    width: "30%"
+  },
+};
 
 const UserDetails = ({ show }: AddressType) => {
   const style = {
@@ -16,12 +30,23 @@ const UserDetails = ({ show }: AddressType) => {
   const [oldPassword, setOldPassword] = useState<any>("")
   const [newPassword, setNewPassword] = useState<any>("")
   const [confirmPassword, setConfirmPassword] = useState<any>("")
-
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+  const [firstName, setFirstName] = useState<any>('');
+  const [lastName, setLastName] = useState<any>('');
+  const [salutation, setSalutation] = useState<any>('');
+  const [gender, setGender] = useState<any>('');
+  const [email, setEmail] = useState<any>('');
   useEffect(() => {
     setAuthToken(localStorage.getItem("token"))
     setUserId(localStorage.getItem("userId"))
   }, [])
-
+  useEffect(() => {
+    setSalutation(userDetails?.salutation);
+    setFirstName(userDetails?.firstName);
+    setLastName(userDetails?.lastName);
+    setEmail(userDetails?.email);
+    setGender(userDetails?.gender);
+  }, [userDetails])
   const getUserDetails = async () => {
     if (authToken) {
       setLoading(true)
@@ -82,6 +107,55 @@ const UserDetails = ({ show }: AddressType) => {
     }
 
   }
+
+  const updateProfile = async () => {
+    const data = {
+      data: {
+        type: "customers",
+        attributes: {
+          firstName: firstName,
+          lastName: lastName,
+          gender: gender,
+          salutation: salutation,
+          email: email,
+          acceptedTerms: true
+        }
+      }
+    }
+    try {
+      const resp = await fetch(`${API_URL}/customers/${userId}`,
+        {
+          method: "PATCH",
+          headers: {
+            "Authorization": `Bearer ${authToken}`
+          },
+          body: JSON.stringify(data)
+        }
+      );
+      const result = await resp.json();
+      console.log("re-up", result)
+      getUserDetails()
+      closeModal()
+    } catch (err) {
+      console.log("err", err)
+    }
+  }
+
+  //for madal implementation
+  let subtitle: any;
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = 'rgb(207, 18, 46)';
+  }
+ 
+  function closeModal() {
+    setIsOpen(false);
+  }
   return (
 
     <div style={style}>
@@ -91,15 +165,26 @@ const UserDetails = ({ show }: AddressType) => {
         </div> :
         <div style={{ display: "flex", justifyContent: "space-between" }}>
           <div style={{ width: "50%", background: "#f2f2f2", padding: "1rem" }}>
-            <h1 style={{
-              fontWeight: "500",
-              fontSize: "1.5rem",
-              lineHeight: "1.4",
-              display: "block",
-              color: "#333",
-              marginBottom: "1rem"
-            }}>Profile</h1>
-            <div style={{ display: "flex", justifyContent: "space-around" }}>
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <h1 style={{
+                fontWeight: "500",
+                fontSize: "1.5rem",
+                lineHeight: "1.4",
+                display: "block",
+                color: "#333",
+                marginBottom: "1rem"
+              }}>Profile</h1>
+              <button style={{
+                width: "85px",
+                fontWeight: "bold",
+                color: "white",
+                background: "rgb(207, 18, 46)",
+                borderRadius: "1px"
+              }} onClick={() => openModal()}>
+                Update
+              </button>
+            </div>
+            <div style={{ display: "flex", justifyContent: "space-around", paddingTop: "10px" }}>
               <div style={{ display: "flex", flexDirection: "column" }}>
                 <p style={{
                   display: "block",
@@ -299,6 +384,220 @@ const UserDetails = ({ show }: AddressType) => {
             </button>
           </div>
         </div>}
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <div>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Update Profile</h2>
+            <button onClick={closeModal} style={{ background: "black", borderRadius: "1px", color: "white", paddingInline: "10px" }}>X</button>
+          </div>
+          <div style={{ paddingTop: "10px" }}>
+            <div style={{ display: "flex", flexDirection: "column", padding: "4px" }}>
+              <label style={{
+                display: "block",
+                fontSize: "0.75rem",
+                fontWeight: "700",
+                marginBottom: "0.4rem",
+                textTransform: "uppercase",
+                color: "#333",
+              }}>Salutation</label>
+              <select style={{
+                display: "block",
+                borderRadius: "2px",
+                border: "0.0625rem solid #dce0e5",
+                color: "#333",
+                background: "#ffffff",
+                font: "400 0.9375rem/2rem 'Circular', sans-serif",
+                padding: "0 0.15rem 0 1.25rem",
+                whiteSpace: "nowrap",
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+              }} value={salutation} onChange={(e) => setSalutation(e.target.value)}>
+                <option value="Mr">Mr.</option>
+                <option value="Ms">Ms.</option>
+                <option value="Mrs">Mrs.</option>
+                <option value="Dr">Dr.</option>
+              </select>
+            </div>
+            <div style={{ display: "flex", flexDirection: "column", padding: "4px" }}>
+              <label style={{
+                display: "block",
+                fontSize: "0.75rem",
+                fontWeight: "700",
+                marginBottom: "0.4rem",
+                textTransform: "uppercase",
+                color: "#333",
+              }}>First Name</label>
+              <input type='text' placeholder='Enter first name'
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                style={{
+                  display: "block",
+                  borderRadius: "2px",
+                  border: "0.0625rem solid #dce0e5",
+                  color: "#333",
+                  background: "#ffffff",
+                  font: "400 0.9375rem/2rem 'Circular', sans-serif",
+                  padding: "0 2.8125rem 0 1.25rem",
+                  whiteSpace: "nowrap",
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                }} />
+            </div>
+            <div style={{ display: "flex", flexDirection: "column", padding: "4px" }}>
+              <label style={{
+                display: "block",
+                fontSize: "0.75rem",
+                fontWeight: "700",
+                marginBottom: "0.4rem",
+                textTransform: "uppercase",
+                color: "#333",
+              }}>Last Name</label>
+              <input type='text' placeholder='Enter last name'
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                style={{
+                  display: "block",
+                  borderRadius: "2px",
+                  border: "0.0625rem solid #dce0e5",
+                  color: "#333",
+                  background: "#ffffff",
+                  font: "400 0.9375rem/2rem 'Circular', sans-serif",
+                  padding: "0 2.8125rem 0 1.25rem",
+                  whiteSpace: "nowrap",
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                }} />
+            </div>
+            <div style={{ display: "flex", flexDirection: "column", padding: "4px" }}>
+              <label style={{
+                display: "block",
+                fontSize: "0.75rem",
+                fontWeight: "700",
+                marginBottom: "0.4rem",
+                textTransform: "uppercase",
+                color: "#333",
+              }}>Gender</label>
+              <div style={{ display: "flex" }}>
+                <div style={{ display: "flex" }}>
+                  <input
+                    type='radio'
+                    name="gender"
+                    value="Male"
+                    onChange={(e) => setGender(e.target.value)}
+                    checked={gender === "Male"}
+                    style={{
+                      display: "block",
+                      borderRadius: "2px",
+                      border: "0.0625rem solid #dce0e5",
+                      color: "#333",
+                      background: "#ffffff",
+                      font: "400 0.9375rem/2rem 'Circular', sans-serif",
+                      padding: "0 2.8125rem 0 1.25rem",
+                      whiteSpace: "nowrap",
+                      overflow: "hidden",
+                      textOverflow: "ellipsis",
+                    }}
+                  />
+                  <label>Male</label>
+                </div>
+                <div style={{ display: "flex" }}>
+                  <input
+                    type='radio'
+                    name="gender"
+                    value="Female"
+                    onChange={(e) => setGender(e.target.value)}
+                    checked={gender === "Female"}
+                    style={{
+                      display: "block",
+                      borderRadius: "2px",
+                      border: "0.0625rem solid #dce0e5",
+                      color: "#333",
+                      background: "#ffffff",
+                      font: "400 0.9375rem/2rem 'Circular', sans-serif",
+                      padding: "0 2.8125rem 0 1.25rem",
+                      whiteSpace: "nowrap",
+                      overflow: "hidden",
+                      textOverflow: "ellipsis",
+                    }}
+                  />
+                  <label>Female</label>
+                </div>
+                <div style={{ display: "flex" }}>
+                  <input
+                    type='radio'
+                    name="gender"
+                    value="Other"
+                    onChange={(e) => setGender(e.target.value)}
+                    checked={gender === "Other"}
+                    style={{
+                      display: "block",
+                      borderRadius: "2px",
+                      border: "0.0625rem solid #dce0e5",
+                      color: "#333",
+                      background: "#ffffff",
+                      font: "400 0.9375rem/2rem 'Circular', sans-serif",
+                      padding: "0 2.8125rem 0 1.25rem",
+                      whiteSpace: "nowrap",
+                      overflow: "hidden",
+                      textOverflow: "ellipsis",
+                    }}
+                  />
+                  <label>Other</label>
+                </div>
+              </div>
+
+
+
+            </div>
+            <div style={{ display: "flex", flexDirection: "column", padding: "4px" }}>
+              <label style={{
+                display: "block",
+                fontSize: "0.75rem",
+                fontWeight: "700",
+                marginBottom: "0.4rem",
+                textTransform: "uppercase",
+                color: "#333",
+              }}>Email</label>
+              <input
+                type='text'
+                placeholder='Enter Email'
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                style={{
+                  display: "block",
+                  borderRadius: "2px",
+                  border: "0.0625rem solid #dce0e5",
+                  color: "#333",
+                  background: "#ffffff",
+                  font: "400 0.9375rem/2rem 'Circular', sans-serif",
+                  padding: "0 2.8125rem 0 1.25rem",
+                  whiteSpace: "nowrap",
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                }} />
+            </div>
+            <div style={{ display: "flex", justifyContent: "center", marginTop: "10px" }}>
+              <button
+                style={{
+                  padding: "10px",
+                  fontWeight: "bold",
+                  color: "white",
+                  background: "rgb(207, 18, 46)",
+                  borderRadius: "1px"
+                }}
+                onClick={() => updateProfile()}
+              >Submit</button>
+            </div>
+          </div>
+        </div>
+
+      </Modal>
     </div>
   )
 }
